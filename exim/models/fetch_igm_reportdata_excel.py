e49f60e2743ac3a49446f30108ec1dae108ab4ef
k1h1a1n <affykhan789678@gmail.com>
2023-01-19 11:34:34 +0000

    single page

--- exim/models/fetch_igm_reportdata_excel.py
+++ exim/models/fetch_igm_reportdata_excel.py
@@ -21,10 +21,6 @@ class IgmXlsx(models.AbstractModel):
             {"name": "Inward Date", "value": str(igm_data.inward_date)},
             {"name": "Quality", "value": str(igm_data.quality)},
             {"name": "Incoterms", "value": str(igm_data.incoterms)},
-            {"name": "CIF VALUE", "value": igm_data.cif_value},
-            {"name": "Landing Cost", "value": igm_data.landing_cost},
-            {"name": "Per KG Cost", "value": igm_data.per_box_cost},
-            {"name": "Per Box Cost", "value": igm_data.pb_cost},
         ]
 
         column_names_1 = [
@@ -73,7 +69,10 @@ class IgmXlsx(models.AbstractModel):
         column_names_4 = [
             {"name": "Claim Amount", "value": igm_data.claim_amount},
             {"name": "Total Net Wt.", "value": igm_data.sum_net_wt},
-            {"name": "Claim Settlement Amount", "value": igm_data.claim_settlement_amount},
+            {
+                "name": "Claim Settlement Amount",
+                "value": igm_data.claim_settlement_amount,
+            },
             {"name": "Actual Weight", "value": igm_data.actual_weight},
             {"name": "Claim Settled", "value": igm_data.claim_settled},
             {"name": "Sample Weight", "value": igm_data.sample_weight},
@@ -96,47 +95,87 @@ class IgmXlsx(models.AbstractModel):
             column_names_4,
         ]
         sheets = [sheet_0, sheet_0, sheet_0, sheet_0, sheet_0]
+        header = workbook.add_format({"bold": True, "font_color": "black"})
+        header.set_font_size(15)
+        # header.set_bg_color('#8C8C8C')
+        value_format = workbook.add_format({"bold": False, "font_color": "black"})
+        value_format.set_font_size(13)
+        cell_format = workbook.add_format({"bold": False, "font_color": "white"})
+        cell_format.set_font_size(14)
+        # cell_format.set_text_wrap()
+        cell_format.set_bg_color("#050138")
+        sheet_0.set_column(0, 16, 18)
+        # sheet_0.set_row(0, 20)
+        number_format = workbook.add_format({"num_format": "₹#,0.00"})
+        number_format.set_font_size(15)
+        summary = workbook.add_format({"bold": True, "font_color": "black"})
+        summary.set_font_size(16)
+        sheet_0.write(2, 0, "General Details", header)
+        sheet_0.write(6, 0, "CIF Line", header)
+        sheet_0.write(16, 0, "Duty Line", header)
+        sheet_0.write(24, 0, "Container", header)
+        sheet_0.write(29, 0, "Release", header)
+        sheet_0.write(36, 7, "CIF VALUE", summary)
+        sheet_0.write(36, 8, igm_data.cif_value, number_format)
+        sheet_0.write(37, 7, "Landing Cost", summary)
+        sheet_0.write(37, 8, igm_data.landing_cost, number_format)
+        sheet_0.write(38, 7, "Per KG Cost", summary)
+        sheet_0.write(38, 8, igm_data.per_box_cost, number_format)
+        sheet_0.write(39, 7, "Per Box Cost", summary)
+        sheet_0.write(39, 8, igm_data.pb_cost, number_format)
+
         # sheet_0.write(0, col_num, column_name["name"])
         for i in range(5):
             for col_num, column_name in enumerate(columns[i]):
                 if i == 0:
-                    sheets[i].write(0, col_num, column_name["name"])
-                    sheets[i].write(1, col_num, column_name["value"])
+                    sheets[i].write(3, col_num, column_name["name"], cell_format)
+                    sheets[i].write(4, col_num, column_name["value"], value_format)
                 if i == 1:
-                    row = 3
+                    row = 7
                     for lines in igm_data.cif_details:
                         row = row + 1
                         x = column_name["value"]
                         y = column_name["name"]
-                        sheets[i].write(3, col_num, column_name["name"])
+                        sheets[i].write(7, col_num, column_name["name"], cell_format)
                         if y == "CI Line Item":
                             sheets[i].write(
-                                row, col_num, lines.ci_product_line_item.product.name
+                                row,
+                                col_num,
+                                lines.ci_product_line_item.product.name,
+                                value_format,
                             )
                         else:
-                            sheets[i].write(row, col_num, getattr(lines, x))
+                            sheets[i].write(
+                                row, col_num, getattr(lines, x), value_format
+                            )
                 if i == 2:
-                    row = 10
+                    row = 17
                     for lines in igm_data.duty_details:
                         row = row + 1
                         x = column_name["value"]
                         y = column_name["name"]
-                        sheets[i].write(10, col_num, column_name["name"])
+                        sheets[i].write(17, col_num, column_name["name"], cell_format)
                         if y == "CI Line Item":
                             sheets[i].write(
-                                row, col_num, lines.ci_product_line_item.product.name
+                                row,
+                                col_num,
+                                lines.ci_product_line_item.product.name,
+                                value_format,
                             )
                         else:
-                            sheets[i].write(row, col_num, getattr(lines, x))
+                            sheets[i].write(
+                                row, col_num, getattr(lines, x), value_format
+                            )
                 if i == 3:
-                    row = 17
+                    row = 25
                     for lines in igm_data.container_details:
                         row = row + 1
                         x = column_name["value"]
                         y = column_name["name"]
-                        sheets[i].write(17, col_num, column_name["name"])
-                        sheets[i].write(row, col_num, str(getattr(lines, x)))
-
+                        sheets[i].write(25, col_num, column_name["name"], cell_format)
+                        sheets[i].write(
+                            row, col_num, str(getattr(lines, x)), value_format
+                        )
                 if i == 4:
-                    sheets[i].write(22, col_num, column_name["name"])
-                    sheets[i].write(23, col_num, column_name["value"])
+                    sheets[i].write(30, col_num, column_name["name"], cell_format)
+                    sheets[i].write(31, col_num, column_name["value"], value_format)
